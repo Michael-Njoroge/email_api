4699419d7c408dbc0d5c7f7f41d36e3f6ad8d160
Michael-Njoroge <mikethecoder12@gmail.com>
2023-10-04 13:10:53 +0300

    email-verification setup

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -7,12 +8,18 @@ use Illuminate\Support\Facades\Route;
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
+| Here is where you can register wre('verifieeb routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| be assigned to the "web" middleware grdoup. Make something great!
 |
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes([
+    'verify' => true
+]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('verified');
